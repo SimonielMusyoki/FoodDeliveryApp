21a51c2840be1b9a97288cb1ec642ab9fd6d50ce
SimonielMusyoki <musyoki.mtk3@gmail.com>
2020-12-09 07:25:11 +0300

    Assets & Confs

---
+++ constants/images.js
@@ -0,0 +1,61 @@
+export const avatar_1 = require("../assets/images/avatar-1.jpg");
+export const avatar_2 = require("../assets/images/avatar-2.jpg");
+export const avatar_3 = require("../assets/images/avatar-3.jpg");
+export const avatar_4 = require("../assets/images/avatar-4.jpg");
+export const avatar_5 = require("../assets/images/avatar-5.jpg");
+export const baked_fries = require("../assets/images/baked-fries.jpg");
+export const burger_restaurant_2 = require("../assets/images/burger-restaurant-2.jpg");
+export const burger_restaurant = require("../assets/images/burger-restaurant.jpg");
+export const chicago_hot_dog = require("../assets/images/chicago-hot-dog.jpg");
+export const crispy_chicken_burger = require("../assets/images/crispy-chicken-burger.jpg");
+export const fries_restaurant = require("../assets/images/fries-restaurant.jpg");
+export const hawaiian_pizza = require("../assets/images/hawaiian-pizza.jpg");
+export const honey_mustard_chicken_burger = require("../assets/images/honey-mustard-chicken-burger.jpg");
+export const hot_dog_restaurant = require("../assets/images/hot-dog-restaurant.jpg");
+export const ice_kacang = require("../assets/images/ice-kacang.jpg");
+export const japanese_restaurant = require("../assets/images/japanese-restaurant.jpg");
+export const kek_lapis_shop = require("../assets/images/kek-lapis-shop.jpg");
+export const kek_lapis = require("../assets/images/kek-lapis.jpg");
+export const kolo_mee = require("../assets/images/kolo-mee.jpg");
+export const nasi_briyani_mutton = require("../assets/images/nasi-briyani-mutton.jpg");
+export const nasi_lemak = require("../assets/images/nasi-lemak.jpg");
+export const noodle_shop = require("../assets/images/noodle-shop.jpg");
+export const pizza_restaurant = require("../assets/images/pizza-restaurant.jpg");
+export const pizza = require("../assets/images/pizza.jpg");
+export const salad = require("../assets/images/salad.jpg");
+export const sarawak_laksa = require("../assets/images/sarawak-laksa.jpg");
+export const sushi = require("../assets/images/sushi.jpg");
+export const teh_c_peng = require("../assets/images/teh-c-peng.jpg");
+export const tomato_pasta = require("../assets/images/tomato-pasta.jpg");
+
+export default {
+    avatar_1,
+    avatar_2,
+    avatar_3,
+    avatar_4,
+    avatar_5,
+    baked_fries,
+    burger_restaurant_2,
+    burger_restaurant,
+    chicago_hot_dog,
+    crispy_chicken_burger,
+    fries_restaurant,
+    hawaiian_pizza,
+    honey_mustard_chicken_burger,
+    hot_dog_restaurant,
+    ice_kacang, 
+    japanese_restaurant,
+    kek_lapis_shop,
+    kek_lapis,
+    kolo_mee,
+    nasi_briyani_mutton,
+    nasi_lemak,
+    noodle_shop,
+    pizza_restaurant,
+    pizza_restaurant,
+    salad,
+    sarawak_laksa,
+    sushi,
+    teh_c_peng,
+    tomato_pasta
+};
---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import icons from './icons';
+import images from './images';
+import { FONTS, COLORS, SIZES } from './theme';
+import GOOGLE_API_KEY from './maps'
+
+export { icons, images, FONTS, COLORS, SIZES, GOOGLE_API_KEY };
